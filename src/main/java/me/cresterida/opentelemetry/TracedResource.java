ce133268df4c9fa296735a4356bedf5a0b7e9023
kiquetal <kiquetal@gmail.com>
2024-04-18 15:18:26 -0400

    Adding printing header http

--- src/main/java/me/cresterida/opentelemetry/TracedResource.java
+++ src/main/java/me/cresterida/opentelemetry/TracedResource.java
@@ -3,6 +3,8 @@ package me.cresterida.opentelemetry;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.MediaType;
 
 import java.util.logging.Logger;
@@ -13,8 +15,9 @@ public class TracedResource
     private static final Logger Log = Logger.getLogger(TracedResource.class.getName());
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello()
+    public String hello(@Context HttpHeaders httpHeaders)
     {
+//        httpHeaders.getRequestHeaders().forEach((k,v) -> Log.info(k + " : " + v));
         Log.info("Hello from traced resource");
         return "Hello from traced resource";
     }
